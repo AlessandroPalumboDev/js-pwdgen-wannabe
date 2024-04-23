8d338d5a899d455a9f37ba10fe1f7c50fbc7d773
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-23 14:54:46 +0200

    [UPD] Modifica della variabile aggiungendo dati utente inseriti

--- js/main.js
+++ js/main.js
@@ -5,3 +5,5 @@ let currentText = document.getElementById('username').innerHTML;
 const userName = prompt('Inserisci il tuo Nome');
 const userLastName = prompt('Inserisci il tuo Cognome');
 const userColor = prompt('Inserisci il tuo Colore preferito');
+
+currentText = currentText + ' ' + userName + ' ' + userLastName + ' ' + ("e il tuo colore preferito è:") + ' ' + userColor;
