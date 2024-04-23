8758f13c80588af7451d47f25795c3ffbcff7f28
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-23 14:24:45 +0200

    [UPD] Creazione prompt su main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const userName = prompt('Inserisci il tuo Nome');
+const userLastName = prompt('Inserisci il tuo Cognome');
+const userColor = prompt('Inserisci il tuo Colore preferito');
