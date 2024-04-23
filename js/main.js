ec2f61e30df2874ec080c1ae7491029336dc1df8
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-23 15:19:05 +0200

    [UPD] Aggiunta variabile per numero casuale

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 "use strict";
 
+// CREAZIONE VARIABILE DA ELEMENTO HTML
 let currentText = document.getElementById('username').innerHTML;
 
+// AGGIUNTA VARIABILE PER CALCOLO NUMERO CASUALE
+let casualNumber = Math.floor(Math.random()*100);
+
+// CREZIONE PROMPT PER INSERIMENTO DATI UTENTE
 const userName = prompt('Inserisci il tuo Nome');
 const userLastName = prompt('Inserisci il tuo Cognome');
 const userColor = prompt('Inserisci il tuo Colore preferito');
 
-currentText = currentText + ' ' + userName + ' ' + userLastName + ' ' + ("e il tuo colore preferito è:") + ' ' + userColor;
+// MODFICA VARIBILE AGGIUNGENDO DATI UTENTE E NUMERO CASUALE
+currentText = currentText + ' ' + userName + ' ' + userLastName + ' ' + ("e il tuo colore preferito è:") + ' ' + userColor + '. ' + ("Visto che ci piace dare i numeri:") + ' ' + '!' + casualNumber + '!';
 
+// STAMPA SU DOCUMENTO VARIABILE MODIFICATA CON DATI UTENTE 
 document.getElementById('username').innerHTML = currentText;
 
+// STAMPA SU CONSOLE VARIABILE MODIFICATA CON DATI UTENTE 
 console.log(currentText);
