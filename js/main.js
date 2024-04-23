c2927a31535898d9f7ae2cced874b79396e3bdec
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-23 14:52:08 +0200

    [UPD] Creazione variabile di un elemento html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let currentText = document.getElementById('username').innerHTML;
+
 const userName = prompt('Inserisci il tuo Nome');
 const userLastName = prompt('Inserisci il tuo Cognome');
 const userColor = prompt('Inserisci il tuo Colore preferito');
