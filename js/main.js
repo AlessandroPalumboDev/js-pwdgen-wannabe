de047f2b1daf82dad2b8759abe0058c845bcc22b
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-04-24 10:31:51 +0200

    [FIX] Cambio Id e nomi variabili

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 "use strict";
 
 // CREAZIONE VARIABILE DA ELEMENTO HTML
-let currentText = document.getElementById('username').innerHTML;
+let currentText = document.getElementById('password').innerHTML;
 
 // AGGIUNTA VARIABILE PER CALCOLO NUMERO CASUALE
 let casualNumber = Math.floor(Math.random()*100);
 
 // CREZIONE PROMPT PER INSERIMENTO DATI UTENTE
-const userName = prompt('Inserisci il tuo Nome');
-const userLastName = prompt('Inserisci il tuo Cognome');
+const firstName = prompt('Inserisci il tuo Nome');
+const lastName = prompt('Inserisci il tuo Cognome');
 const userColor = prompt('Inserisci il tuo Colore preferito');
 
 // MODFICA VARIBILE AGGIUNGENDO DATI UTENTE E NUMERO CASUALE
-currentText = currentText + userName + userLastName + userColor + casualNumber;
+currentText = currentText + firstName + lastName + userColor + casualNumber;
 
 // STAMPA SU DOCUMENTO VARIABILE MODIFICATA CON DATI UTENTE 
-document.getElementById('username').innerHTML = currentText;
+document.getElementById('password').innerHTML = currentText;
 
 // STAMPA SU CONSOLE VARIABILE MODIFICATA CON DATI UTENTE 
 console.log(currentText);
